f2364ddf3fa1bb983f6411b0a8e4731e197b7d88
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-06-23 15:36:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "terraform-bucket"
+  default     = "bucket-created-by-terraform-code-when-it-ran-in-cicd"
 }
 
 variable "acl" {
