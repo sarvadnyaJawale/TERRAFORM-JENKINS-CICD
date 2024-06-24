92b7f2a14ce854986a69447478c29c7d726a0ace
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-06-24 07:16:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+  default     = "ap-south-1"
+}
 
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
