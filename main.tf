84ed59314cfb8d78baeb6ba1f7cb01a60e480140
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-06-24 06:16:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,57 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = "tf-example"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.my_vpc.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.my_vpc.id
+ route {
+   cidr_block = "0.0.0.0/0" //route to internet through igw
+   gateway_id = aws_internet_gateway.gw.id
+ }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "ap-south-1"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ subnet_id      = aws_subnet.my_subnet.id 
+ route_table_id = aws_route_table.second_rt.id
+}
+
 resource "aws_instance" "Ajay" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
+  ami                    = "ami-0f58b397bc5c1f2e8"
+  instance_type          = "t2.medium"
+  key_name               = "sarva"
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   user_data              = base64encode(file("website.sh"))
   tags = {
     Name = "Terraform-EC2"
   }
 }
-
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22 and 443"
