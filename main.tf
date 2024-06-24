b71f4a3c9df74a1b6c8e8a4e73f3c708e07320ee
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-06-24 08:35:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "tf-example"
