8d986f46eb95141ceba22f151bc80ab82b96b3e5
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-06-24 07:36:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "tf-example"
   }
@@ -21,19 +21,11 @@ resource "aws_route_table" "second_rt" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 
   tags = {
     Name = "tf-example"
-  }
-}
-
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-  tags = {
-    Name = "primary_network_interface"
   }
 }
 
